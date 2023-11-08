1104c76f562177a38df2e047ce3322c87d9b8ff4
MiguelMO2311 <mmeneses73@gmail.com>
2023-11-08 21:16:48 +0100

    'ok'

--- Retos1/algebraProject/pointTest.ts
+++ Retos1/algebraProject/pointTest.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Point } from './point'
 
 let p1 = new Point(5.3, 2.1);
@@ -5,3 +6,11 @@ p1.getX
 p1.getY
 console.log(p1.getX, p1.getY);
 p1.toString();
+=======
+import { Point } from "./point";
+
+let point1 = new Point(1,2)
+console.log(point1.toString())
+
+export { Point };
+>>>>>>> repaso2
