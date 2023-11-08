ea910fd6b5d074e9306162a22ca984c7d8095563
MiguelMO2311 <mmeneses73@gmail.com>
2023-11-08 21:17:11 +0100

    'ok'

--- Retos1/algebraProject/pointTest.js
+++ Retos1/algebraProject/pointTest.js
@@ -1,8 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+<<<<<<< HEAD
 var point_1 = require("./point");
 var p1 = new point_1.Point(5.3, 2.1);
 p1.getX;
 p1.getY;
 console.log(p1.getX, p1.getY);
 p1.toString();
+=======
+exports.Point = void 0;
+var point_1 = require("./point");
+Object.defineProperty(exports, "Point", { enumerable: true, get: function () { return point_1.Point; } });
+var point1 = new point_1.Point(1, 2);
+console.log(point1.toString());
+>>>>>>> repaso2
